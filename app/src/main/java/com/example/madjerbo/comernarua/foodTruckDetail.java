6a84a559c2f8f6c7d7255090fa6083eee265db0f
Madjer B oliveira <madjerbo@gmail.com>
2015-11-22 20:15:46 -0300

    Ajuste na activity detail

---
+++ app/src/main/java/com/example/madjerbo/comernarua/foodTruckDetail.java
@@ -0,0 +1,13 @@
+package com.example.madjerbo.comernarua;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class foodTruckDetail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_food_truck_detail);
+    }
+}
